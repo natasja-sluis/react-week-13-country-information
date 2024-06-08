405c55f73d9553e510309f70920e3a4a07a41667
natasja-sluis <natasjag92@gmail.com>
2024-06-08 19:19:33 +0200

    dispay countries on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
 function App() {
 
+    const [countries, setCountries] = React.useState([]);
+
+    async function fetchData() {
+        try {
+            const response = await axios.get('https://restfulcountries.com/api/v1/countries', {
+                headers: {'Authorization': 'Bearer 1076|p34malEeAzxVHNRgAxOAe3XtjxpIHg19oQVwML39'},
+            })
+            setCountries(response.data.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
-            Maak je applicatie hier!
+            <img src="/src/assets/world_map.png" alt="World Map"/>
+            <h1>World Regions</h1>
+            <ul className="country-list">{countries.map((country) => {
+                return <li key={country.name}>
+                    <div className="image-flag-container">
+                        <img src={country.href.flag} alt={country.name}/>
+                        <h3>{country.name}</h3>
+                    </div>
+                    <div className="population-description">Has a population of <div>
+                        <strong>{country.population}</strong></div> people
+                    </div>
+                </li>
+            })}</ul>
+            <button type="button" onClick={fetchData}>Click me</button>
         </>
     )
 }
---
+++ src/helpers/color-names.js
@@ -0,0 +1,28 @@
+
+// RED
+
+//Asia === #9e1b32
+
+
+// YELLOW
+
+//Europe === #FEBE10
+
+// BLUE
+
+//Africa === #007FFF
+
+// GREENS
+
+//The Caribean === #00563B
+
+//South America === #006400
+
+//North America === #17B169
+
+//Central America === #3EB489
+
+
+// PURPLE
+
+// Australia === #33006F
