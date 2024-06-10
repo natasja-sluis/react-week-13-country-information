d6bd4a4b64f47738f3fc0f9a25cefb92b394e58b
natasja-sluis <natasjag92@gmail.com>
2024-06-10 12:06:52 +0200

    added search function

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,22 @@ function App() {
 
     const [countries, setCountries] = React.useState([]);
     const [singleCountry, setSingleCountry] = React.useState({});
+    const [loaded, setLoaded] = React.useState(false);
+    const [countryValue, setCountryValue] = React.useState("");
+    const [error, toggleError] = React.useState(false);
 
-    async function fetchSingleCountryData() {
+    async function fetchSingleCountryData(event) {
+        toggleError(false)
+        setLoaded(false)
         try {
-            const response = await axios.get('https://restfulcountries.com/api/v1/countries/Iceland', {
+            event.preventDefault();
+            const response = await axios.get(`https://restfulcountries.com/api/v1/countries/${countryValue}`, {
                 headers: {'Authorization': 'Bearer 1076|p34malEeAzxVHNRgAxOAe3XtjxpIHg19oQVwML39'},
             })
             setSingleCountry(response.data.data);
+            setLoaded(true);
         } catch (error) {
-            console.log(error);
+            toggleError(true);
         }
     }
 
@@ -47,17 +54,32 @@ function App() {
             <img src="/src/assets/world_map.png" alt="World Map"/>
             <h2>Search Country Information</h2>
 
-                <button type="submit" onClick={fetchSingleCountryData}>Search</button>
+            <form onSubmit={fetchSingleCountryData}>
+                <label htmlFor="search-country">
+                    <input
+                        className="input-field-search"
+                        type="text"
+                        name="search-country"
+                        id="search-country"
+                        placeholder="Please enter a country..."
+                        value={countryValue}
+                        onChange={(event) => setCountryValue(event.target.value)}
+                    />
+                </label>
+                <button type="submit">Search</button>
+            </form>
 
-
-            <div>
+            {loaded ? <div className="country-information">
                 <div className="image-flag-container">
                     <img src={singleCountry.href.flag} alt={singleCountry.name}/>
                     <h3>{singleCountry.name}</h3>
                 </div>
-                <p>{singleCountry.name} is situated in {singleCountry.continent} and the capital
-                    is {singleCountry.capital}. It has a population of {roundNumber(singleCountry.population)} million.</p>
-            </div>
+                        <p className="country-description">{singleCountry.name} is situated in {singleCountry.continent} and the capital
+                            is {singleCountry.capital}. It has a population
+                            of {roundNumber(singleCountry.population)} million.</p>
+            </div> : ""}
+            {error && <p className="error-message">Sorry this country does not exist. Please try again.</p>}
+
             <h2>World Regions</h2>
             {countries.length < 1 && <button type="button" onClick={fetchCountriesData}>Click me</button>}
             <ul className="country-list">{countries.map((country) => {
@@ -66,7 +88,7 @@ function App() {
                             <img src={country.href.flag} alt={country.name}/>
                             <h3 className={assignClassName(country.continent)}>{country.name}</h3>
                         </div>
-                        <div className="population-description">Has a population of <div>
+                        <div className="country-description">Has a population of <div>
                             <strong>{country.population}</strong></div> people
                         </div>
                     </li>
